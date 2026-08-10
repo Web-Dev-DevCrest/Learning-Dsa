74fbd2c0c594048be20d1af55c894b3c8b864b41
Devansh Modi <devanshmodi250@gmail.com>
2026-08-10 21:33:52 +0530

    Added improved exp method

--- recursions/exponent.cpp
+++ recursions/exponent.cpp
@@ -1,13 +1,34 @@
 #include <iostream>
 
+int x{0};
 int exp(int m , int n){
+    x++;
     if(n==0){
         return 1;
     }
     return exp(m,n-1)*m;
+//This is order of n . 
+}
+
+//Improved method
+int y {0};
+int expImproved(int m , int n ){
+    y++;
+    if (n==0){
+        return 1;
+    }
+    if(n%2==0){
+        return expImproved(m*m,n/2);
+    }
+    
+    return (m * expImproved(m*m,(n-1)/2));
 }
 int main(){
-    int ans = exp(2,5);
-    std::cout<<ans<<std::endl;
+
+    int ans = exp(2,16);
+    std::cout<<ans<<" Iterations taken = "<<x<<std::endl;
+    int ans2 = expImproved(2,16);
+    std::cout<<ans2<<" Iterations taken in 2nd = "<<y<<std::endl;
     return 0;
+
 }
